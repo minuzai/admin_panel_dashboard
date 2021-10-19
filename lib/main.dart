2c01305cd844fe337793a53faf6ed8328e51d8fb
Min Jae Kim <exploit017@gmail.com>
2021-10-19 14:42:47 +0900

    widget tree 위젯 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:admin_panel_dashboard/widget_tree.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,12 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo'),
-        ),
-        body: SizedBox(),
-      ),
+      home: WidgetTree(),
     );
   }
 }
---
+++ lib/widget_tree.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  @override
+  _WidgetTreeState createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Widget Tree'),
+      ),
+    );
+  }
+}
