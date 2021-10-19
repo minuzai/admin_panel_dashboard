c0ff02a38ef5619af68f37bdcc46ea63c61b46ac
Min Jae Kim <exploit017@gmail.com>
2021-10-19 15:23:45 +0900

    코드 정리

--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:admin_panel_dashboard/responsive_layout.dart';
+
 class WidgetTree extends StatefulWidget {
   @override
   _WidgetTreeState createState() => _WidgetTreeState();
@@ -9,8 +11,19 @@ class _WidgetTreeState extends State<WidgetTree> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Widget Tree'),
+      appBar: PreferredSize(
+        child: ResponsiveLayout.isTiny(context) ||
+                ResponsiveLayout.isTinyHeight(context)
+            ? Container()
+            : AppBar(),
+        preferredSize: Size(double.infinity, 100),
+      ),
+      body: ResponsiveLayout(
+        tiny: Container(),
+        phone: Container(),
+        tablet: Container(),
+        largeTablet: Container(),
+        computer: Container(),
       ),
     );
   }
