64b06f580bd3f82b9cd182eec739d344f83b58f6
Min Jae Kim <exploit017@gmail.com>
2021-10-19 16:55:52 +0900

    drawer 위젯 수정

--- lib/drawer/drawer_page.dart
+++ lib/drawer/drawer_page.dart
@@ -4,11 +4,6 @@ import 'package:flutter/rendering.dart';
 
 import 'package:admin_panel_dashboard/constants.dart';
 
-class DrawerPage extends StatefulWidget {
-  @override
-  _DrawerPageState createState() => _DrawerPageState();
-}
-
 class ButtonsInfo {
   String title;
   IconData icon;
@@ -20,76 +15,91 @@ int _currentIndex = 0;
 
 List<ButtonsInfo> _buttonNames = [
   ButtonsInfo(title: 'Home', icon: Icons.home),
+  ButtonsInfo(title: 'Setting', icon: Icons.settings),
+  ButtonsInfo(title: 'Notifications', icon: Icons.notifications),
+  ButtonsInfo(title: 'Contacts', icon: Icons.contact_phone_rounded),
+  ButtonsInfo(title: 'Sales', icon: Icons.sell),
+  ButtonsInfo(title: 'Marketing', icon: Icons.mark_email_read),
+  ButtonsInfo(title: 'Security', icon: Icons.verified_user),
+  ButtonsInfo(title: 'Users', icon: Icons.supervised_user_circle_rounded),
 ];
+
+class DrawerPage extends StatefulWidget {
+  @override
+  _DrawerPageState createState() => _DrawerPageState();
+}
 
 class _DrawerPageState extends State<DrawerPage> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(Constants.kPadding),
-        child: Column(
-          children: [
-            ListTile(
-              title: Text(
-                'Admin Menu',
-                style: TextStyle(color: Colors.white),
-              ),
-              trailing: ResponsiveLayout.isComputer(context)
-                  ? null
-                  : IconButton(
-                      icon: Icon(Icons.close, color: Colors.white),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-            ),
-            ...List.generate(
-              _buttonNames.length,
-              (index) => Column(
-                children: [
-                  Container(
-                    decoration: index == _currentIndex
-                        ? BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            gradient: LinearGradient(
-                              colors: [
-                                Constants.redDark.withOpacity(0.9),
-                                Constants.orangeDark.withOpacity(0.9),
-                              ],
-                            ),
-                          )
-                        : null,
-                    child: ListTile(
-                      title: Text(
-                        _buttonNames[index].title,
-                        style: TextStyle(color: Colors.white),
+    return Drawer(
+      elevation: 0,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(Constants.kPadding),
+          child: Column(
+            children: [
+              ListTile(
+                title: Text(
+                  'Admin Menu',
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: ResponsiveLayout.isComputer(context)
+                    ? null
+                    : IconButton(
+                        icon: Icon(Icons.close, color: Colors.white),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
                       ),
-                      leading: Padding(
-                        padding: const EdgeInsets.all(Constants.kPadding),
-                        child: Icon(
-                          _buttonNames[index].icon,
-                          color: Colors.white,
+              ),
+              ...List.generate(
+                _buttonNames.length,
+                (index) => Column(
+                  children: [
+                    Container(
+                      decoration: index == _currentIndex
+                          ? BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              gradient: LinearGradient(
+                                colors: [
+                                  Constants.redDark.withOpacity(0.9),
+                                  Constants.orangeDark.withOpacity(0.9),
+                                ],
+                              ),
+                            )
+                          : null,
+                      child: ListTile(
+                        title: Text(
+                          _buttonNames[index].title,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        leading: Padding(
+                          padding: const EdgeInsets.all(Constants.kPadding),
+                          child: Icon(
+                            _buttonNames[index].icon,
+                            color: Colors.white,
+                          ),
+                        ),
+                        onTap: () {
+                          setState(() {
+                            _currentIndex = index;
+                          });
+                        },
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
                         ),
                       ),
-                      onTap: () {
-                        setState(() {
-                          _currentIndex = index;
-                        });
-                      },
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
                     ),
-                  ),
-                  Divider(
-                    color: Colors.white,
-                    thickness: 0.1,
-                  ),
-                ],
+                    Divider(
+                      color: Colors.white,
+                      thickness: 0.1,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
