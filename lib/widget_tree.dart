59309f3744b2f373be97c247739e6633382c3a09
Min Jae Kim <exploit017@gmail.com>
2021-10-19 17:24:32 +0900

    panel 페이지 붙이기

--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -1,4 +1,7 @@
 import 'package:admin_panel_dashboard/drawer/drawer_page.dart';
+import 'package:admin_panel_dashboard/panel_center/panel_center_page.dart';
+import 'package:admin_panel_dashboard/panel_left/panel_left_page.dart';
+import 'package:admin_panel_dashboard/panel_right/panel_right_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:admin_panel_dashboard/responsive_layout.dart';
@@ -21,10 +24,22 @@ class _WidgetTreeState extends State<WidgetTree> {
       ),
       body: ResponsiveLayout(
         tiny: Container(),
-        phone: Container(),
-        tablet: Container(),
-        largeTablet: Container(),
-        computer: Container(),
+        phone: PanelCenterPage(),
+        tablet: Row(children: [
+          Expanded(child: PanelLeftPage()),
+          Expanded(child: PanelCenterPage()),
+        ]),
+        largeTablet: Row(children: [
+          Expanded(child: PanelLeftPage()),
+          Expanded(child: PanelCenterPage()),
+          Expanded(child: PanelRightPage()),
+        ]),
+        computer: Row(children: [
+          Expanded(child: DrawerPage()),
+          Expanded(child: PanelLeftPage()),
+          Expanded(child: PanelCenterPage()),
+          Expanded(child: PanelRightPage()),
+        ]),
       ),
       drawer: DrawerPage(),
     );
