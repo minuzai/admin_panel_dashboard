9253207c81c22672dd8720d585e96e47f7f8df9e
Min Jae Kim <exploit017@gmail.com>
2021-10-19 16:27:00 +0900

    drawer 메뉴 레이아웃 및 ui 구현

--- lib/drawer/drawer_page.dart
+++ lib/drawer/drawer_page.dart
@@ -9,6 +9,19 @@ class DrawerPage extends StatefulWidget {
   _DrawerPageState createState() => _DrawerPageState();
 }
 
+class ButtonsInfo {
+  String title;
+  IconData icon;
+
+  ButtonsInfo({required this.title, required this.icon});
+}
+
+int _currentIndex = 0;
+
+List<ButtonsInfo> _buttonNames = [
+  ButtonsInfo(title: 'Home', icon: Icons.home),
+];
+
 class _DrawerPageState extends State<DrawerPage> {
   @override
   Widget build(BuildContext context) {
@@ -30,6 +43,51 @@ class _DrawerPageState extends State<DrawerPage> {
                         Navigator.pop(context);
                       },
                     ),
+            ),
+            ...List.generate(
+              _buttonNames.length,
+              (index) => Column(
+                children: [
+                  Container(
+                    decoration: index == _currentIndex
+                        ? BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: LinearGradient(
+                              colors: [
+                                Constants.redDark.withOpacity(0.9),
+                                Constants.orangeDark.withOpacity(0.9),
+                              ],
+                            ),
+                          )
+                        : null,
+                    child: ListTile(
+                      title: Text(
+                        _buttonNames[index].title,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      leading: Padding(
+                        padding: const EdgeInsets.all(Constants.kPadding),
+                        child: Icon(
+                          _buttonNames[index].icon,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = index;
+                        });
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.white,
+                    thickness: 0.1,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
