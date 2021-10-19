d85158d8cfa97caf4027fa3d970a46e2e0119a06
Min Jae Kim <exploit017@gmail.com>
2021-10-19 16:26:37 +0900

    주요 색상 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:admin_panel_dashboard/widget_tree.dart';
 import 'package:flutter/material.dart';
+
+import 'package:admin_panel_dashboard/constants.dart';
+import 'package:admin_panel_dashboard/widget_tree.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Constants.purpleDark,
+        canvasColor: Constants.purpleLight,
+      ),
       home: WidgetTree(),
     );
   }
